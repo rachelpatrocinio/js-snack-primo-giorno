c83349479d9f1a2c5d950373c0175cb21e125eab
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-03-03 18:49:28 +0100

    Exercises Done

---
+++ js/app.js
@@ -0,0 +1,45 @@
+//Esercizio 1
+//Dichiara una variabile chiamata number e assegnale il valore 100. Stampa il valore della variabile in console.
+const number = 100; //num
+
+console.log(number);
+
+
+//Esercizio 2
+//Dichiara una costante chiamata PI e assegnale il valore di 3.14. Stampa il valore della costante in console.
+const PI = 3.14;  //num
+
+console.log(PI);
+
+
+//Esercizio 2/B
+//Prova ad assegnare alla costante chiamata PI il valore di 3.1416. Sai dire cosa succede?
+//PI = 3.1416; //num
+
+//console.log(PI);
+// come risposta nella console abbiamo un errore perchè usando "const" non possiamo riassegnare un nuovo valore. Se avessimo usato "let", invece, avremmo potuto riassegnare un nuovo valore.
+
+
+//Esercizio 2/C
+//Dichiara una variabile chiamata radius e assegnale il valore 8.
+//Moltiplica radius x 2 e x PI e assegna il valore a una variabile di nome circle.
+//Stampa il valore della variabile circle in console.
+const radius = 8; //num
+const circle = radius * 2 * PI; //num
+
+console.log(circle);
+
+
+// Esercizio 3
+// Dichiara una variabile chiamata name e assegnale il valore “Carlo”.
+// Successivamente, modifica il valore della variabile in “Marco”.
+// Stampa il nuovo valore in console.
+let name = "Carlo"; //string
+name = "Marco"; //string
+
+console.log(name);
+
+
+
+
+
